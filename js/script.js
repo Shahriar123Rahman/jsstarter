22078420a29588108712b776925043afac2a1d91
Shahriar123Rahman <60223904+Shahriar123Rahman@users.noreply.github.com>
2020-10-23 04:13:36 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function productPrice(){
+var x = 15000;
+document.getElementById("demo").innerHTML = x;
+
+
+}
+function productPrices(){
+var x = 16000;
+document.getElementById("demo1").innerHTML = x;
+
+
+}
+function productPrices1(){
+var x = 17000;
+document.getElementById("demo2").innerHTML = x;
+
+
+}
+function productPrices2(){
+var x = 20000;
+document.getElementById("demo3").innerHTML = x;
+
+
+}
+
+
